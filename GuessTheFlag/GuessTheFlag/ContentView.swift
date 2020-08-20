f5d0e2d823d507614b062f3955223c06a9615ba2
Tri <30053202+pham2974@users.noreply.github.com>
2020-08-20 10:22:08 +0700

    HW2

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Tri Pham on 8/19/20.
+//  Copyright © 2020 Tri Pham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
